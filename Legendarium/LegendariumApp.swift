78bb16b7031a600c60c80c65c9faa79170a4b9eb
Jay Long <jaylong255@gmail.com>
2025-03-27 02:35:53 -0500

    hello world

---
+++ Legendarium/LegendariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegendariumApp.swift
+//  Legendarium
+//
+//  Created by Jay Long on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct LegendariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LegendariumTests/LegendariumTests.swift
@@ -0,0 +1,17 @@
+//
+//  LegendariumTests.swift
+//  LegendariumTests
+//
+//  Created by Jay Long on 3/27/25.
+//
+
+import Testing
+@testable import Legendarium
+
+struct LegendariumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
